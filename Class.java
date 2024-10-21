e6461e980bee7e7663e0a0a888560999b200062d
Shaikh Sadik <shaikhali07861@gmail.com>
2024-10-21 20:05:49 +0530

    Instance member function or Static member function

---
+++ Class.java
@@ -0,0 +1,32 @@
+class Class {
+    private int x;
+    private int y;
+    static int c;
+
+    void fun1() {
+        x = 10;
+        y = 20;
+        c=99;
+    }
+
+    void fun2() {
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(c);
+    }
+    void fun3 ()
+    {
+        c=45;
+    }
+
+    public static void main(String[] args) {
+        Class c1 = new Class();
+        Class c2 = new Class();
+        c1.fun2();
+        c1.fun1();
+        c1.fun2();
+        c2.fun3();
+        c2.fun2();
+
+    }
+}
