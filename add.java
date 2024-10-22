c471164addce407b8b006b66f1edbfb060699295
Shaikh Sadik <shaikhali07861@gmail.com>
2024-10-22 17:17:18 +0530

    made some changes

--- add.java
+++ add.java
@@ -4,7 +4,8 @@ public class add {
         System.out.println("Addition OF Two Numbers");
         System.out.println("First Number is "+5);
         System.out.println("Second Number is "+10);
-        System.out.println("The Addition is : "+5+10);
+        System.out.print("The Addition is : ");
+        System.out.println(5+10);
     }
     
     public static void main(String[] args) {
