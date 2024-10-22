ac05230b8268b9b81b6e6c98a3c53fe29e7e4fdc
Shaikh Sadik <shaikhali07861@gmail.com>
2024-10-22 16:35:49 +0530

    The journy to lear java is begin

---
+++ main.java
@@ -0,0 +1,16 @@
+public class main {
+
+    void display(){
+
+        System.out.println("Hello This is my second Java Programme") ;
+        System.out.print("The process of learning Java from "); // here i romve the ln to join next line here
+        System.out.println("Scratch are continued");
+        System.err.println("The java is a literally are amazing "); // here i used err for printing
+    }
+
+    public static void main (String [] args)
+    {
+        main m1=new main();
+        m1.display();
+    }
+}
